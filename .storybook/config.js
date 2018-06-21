673f9789c66e7b912c0d32cd0d8b9369014cca11
Matt Kane <ascorbic@gmail.com>
2018-06-21 17:35:29 +0100

    Add css modules and typings to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,5 @@
 import { configure } from "@storybook/react";
-
+import "../src/scss/base-theme.scss";
 const req = require.context("../src/components", true, /\.story\.[tj]sx?$/);
 
 function loadStories() {

c18d9ceb6148ea0fba114055cefd9157804a01a8
sunya <sunya.dickman@aerian.com>
2018-06-26 20:06:43 +0100

    404 use PageHeader

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { HeroBlock } from "../components/HeroBlock/HeroBlock";
 import Layout from "../components/Layout";
+import { PageHeader } from "../components/PageHeader/PageHeader";
 
 export default () => (
     <Layout location={location}>
-        <HeroBlock>
+        <PageHeader>
             <h1>NOT FOUND</h1>
-        </HeroBlock>
+        </PageHeader>
     </Layout>
 );

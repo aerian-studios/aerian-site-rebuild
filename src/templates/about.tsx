a1edf692ceccdcdeb8ea52183b064fb5a015eb95
Matt Kane <ascorbic@gmail.com>
2018-06-28 07:04:39 +0100

    Make about page

--- src/templates/about.tsx
+++ src/templates/about.tsx
@@ -1,24 +1,51 @@
+import { graphql } from "gatsby";
 import * as React from "react";
-import Layout from "../components/Layout";
+import { FullScreenMedia } from "../components/FullScreenMedia";
+import { PageHeader } from "../components/PageHeader";
+import { isImageSharp } from "../lib/helpers";
+import { About } from "../types/data";
 
-interface Props {
-    style?: React.CSSProperties;
-    className?: string;
-    location: {
-        pathname: string;
-    };
+interface GraphData {
+    pagesJson: About;
 }
 
-export const about: React.SFC<Props> = ({
-    children,
-    style,
-    className,
-    location
-}) => (
-    <Layout location={location}>
-        <main className={[className, "layout-grid"].join(" ")} style={style}>
-            {children}
-        </main>
-    </Layout>
-);
+interface Props {
+    data: GraphData;
+}
+
+export const about: React.SFC<Props> = props => {
+    const { title, heroImage } = props.data.pagesJson;
+    return (
+        <section className="section section--about">
+            <PageHeader>
+                {heroImage && isImageSharp(heroImage) ? (
+                    <FullScreenMedia
+                        image={heroImage.childImageSharp.sizes}
+                        altText={title}
+                        video=""
+                    />
+                ) : (
+                    // Cover the situation where there is no imageSharp (e.g. in the cms)
+                    <img
+                        className="full-screen"
+                        src={heroImage}
+                        alt=""
+                        aria-hidden="true"
+                    />
+                )}
+                <div className="block--hero__content-wrap">
+                    <h1 className="block--hero__title">{title}</h1>
+                </div>
+            </PageHeader>
+        </section>
+    );
+};
+
+export const pageQuery = graphql`
+    query AboutPage($id: String!) {
+        pagesJson(id: { eq: $id }) {
+            ...PageFields
+        }
+    }
+`;
 export default about;

4bcdf66dab68fe8f3acda894de6ecb16b3011dfe
sunya <sunya.dickman@aerian.com>
2018-06-28 10:54:58 +0100

    tests, trying to get it working

--- src/components/MainNavigation/MainNavigation.test.tsx
+++ src/components/MainNavigation/MainNavigation.test.tsx
@@ -5,35 +5,47 @@ import * as renderer from "react-test-renderer";
 
 import { MainNavigation } from "./index";
 
-const pages = {allPagesJson:{
-    edges: [
-        {
-            id: 'page1',
-            path: '/page1',
-            title: 'Page 1'
-        },
-        {
-            id: 'page2',
-            path: '/page2',
-            title: 'Page 2'
-        },
-    ]
-}}
+const pages = {
+    allPagesJson: {
+        edges: [
+            {
+                id: "page1",
+                path: "/page1",
+                title: "Page 1"
+            },
+            {
+                id: "page2",
+                path: "/page2",
+                title: "Page 2"
+            }
+        ]
+    }
+};
 
-describe("MainNavigation", () =>
+describe("MainNavigation", () => {
     it("renders correctly", () => {
         const tree = renderer
-            .create(<MainNavigation className="myClass" pages={pages} activePath={pages.allPagesJson.edges[1].path} />)
+            .create(
+                <MainNavigation
+                    className="myClass"
+                    pages={pages}
+                    activePath={pages.allPagesJson.edges[1].path}
+                />
+            )
             .toJSON();
         expect(tree).toMatchSnapshot();
     });
 
     it("sets '.active-path' to the matching menu item", () => {
         const tree = renderer
-            .create(<MainNavigation className="myClass" pages={pages} activePath={pages.allPagesJson.edges[1].path} />)
+            .create(
+                <MainNavigation
+                    className="myClass"
+                    pages={pages}
+                    activePath={pages.allPagesJson.edges[1].path}
+                />
+            )
             .toJSON();
         expect(tree).toMatchSnapshot();
-
-        console.log(tree)
-    })
-);
+    });
+});

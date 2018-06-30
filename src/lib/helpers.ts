6a37a0e13a5de19cc750d82921f8964016b12670
Matt Kane <ascorbic@gmail.com>
2018-06-30 09:41:50 +0100

    Add fixtures for all types

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,5 +1,5 @@
+import deepMap from "deep-map";
 import { ImageSharp, ImageSharpSizes } from "../types/data";
-
 export const isImageSharp = (
     image: ImageSharp | string
 ): image is ImageSharp => {
@@ -10,4 +10,14 @@ export const isImageSharpSizes = (
     image: ImageSharpSizes | string
 ): image is ImageSharpSizes => {
     return typeof image !== "string";
+};
+
+export const absolutifyURL = (url: string) => {
+    if (typeof url !== "string" || url.substr(0, 8) !== "/assets/") {
+        return url;
+    }
+    return `https://raw.githubusercontent.com/aerian-studios/aerian-site-rebuild/master/static${url}`;
+};
+export const absolutifyURLs = <T = any>(obj: T) => {
+    return deepMap<T>(obj, absolutifyURL);
 };

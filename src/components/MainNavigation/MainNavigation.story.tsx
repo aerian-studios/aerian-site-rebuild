ce24248fd4622a878c02428be2d63f4b7be8edef
Matt Kane <ascorbic@gmail.com>
2018-07-02 16:27:54 +0100

    Get storybooks working with Link components too

--- src/components/MainNavigation/MainNavigation.story.tsx
+++ src/components/MainNavigation/MainNavigation.story.tsx
@@ -1,31 +1,33 @@
-// import * as React from "react";
+import * as React from "react";
 
-// import { storiesOf } from "@storybook/react";
+import { storiesOf } from "@storybook/react";
 
-// import { PageListNode } from "../../types/data";
-// import { MainNavigation } from "./index";
+import { MemoryRouter } from "react-router-dom";
+import { PageListNode } from "../../types/data";
+import { MainNavigation } from "./index";
 
-// const pages: PageListNode[] = [
-//     {
-//         node: {
-//             id: "page1",
-//             path: "/page1",
-//             title: "Page 1"
-//         }
-//     },
-//     {
-//         node: {
-//             id: "page2",
-//             path: "/page2",
-//             title: "Page 2"
-//         }
-//     }
-// ];
+const pages: PageListNode[] = [
+    {
+        node: {
+            id: "page1",
+            path: "/page1",
+            title: "Page 1"
+        }
+    },
+    {
+        node: {
+            id: "page2",
+            path: "/page2",
+            title: "Page 2"
+        }
+    }
+];
 
-// storiesOf("MainNavigation", module)
-//     .add("Page 1 active", () => (
-//         <MainNavigation className="myClass" pages={pages} activePath="/page1" />
-//     ))
-//     .add("Page 2 active", () => (
-//         <MainNavigation className="myClass" pages={pages} activePath="/page2" />
-//     ));
+storiesOf("MainNavigation", module)
+    .addDecorator(story => <MemoryRouter>{story()}</MemoryRouter>)
+    .add("Page 1 active", () => (
+        <MainNavigation className="myClass" pages={pages} activePath="/page1" />
+    ))
+    .add("Page 2 active", () => (
+        <MainNavigation className="myClass" pages={pages} activePath="/page2" />
+    ));

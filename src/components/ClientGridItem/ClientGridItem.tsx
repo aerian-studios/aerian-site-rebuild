f26224f36a4dbdc5f964c55854536842bd11a725
Matt Kane <ascorbic@gmail.com>
2018-07-04 14:48:10 +0100

    Add stub client grid components

---
+++ src/components/ClientGridItem/ClientGridItem.tsx
@@ -0,0 +1,24 @@
+import { Link } from "gatsby";
+import * as React from "react";
+import { Client } from "../../types/data";
+import { Image } from "../Image";
+import * as styles from "./ClientGridItem.scss";
+interface Props {
+    client: Client;
+    style?: React.CSSProperties;
+    className?: string;
+}
+
+export const ClientGridItem: React.SFC<Props> = ({
+    client,
+    style,
+    className
+}) => (
+    <div className={[styles.component, className].join(" ")} style={style}>
+        {client.featuredProject && (
+            <Link to={`/projects/${client.featuredProject}`}>Project</Link>
+        )}
+        <Image source={client.logo} />
+    </div>
+);
+export default ClientGridItem;

46f66369812e30177a58e17ba62e2323c262a360
Matt Kane <ascorbic@gmail.com>
2018-07-06 15:31:10 +0100

    Fix carousel tests

--- src/components/ShowcaseCarousel/ShowcaseCarousel.test.tsx
+++ src/components/ShowcaseCarousel/ShowcaseCarousel.test.tsx
@@ -3,13 +3,18 @@ import * as React from "react";
 
 import * as renderer from "react-test-renderer";
 
+import { MemoryRouter } from "react-router";
 import { project } from "../../types/fixtures";
 import { ShowcaseCarousel } from "./index";
 
 describe("ShowcaseCarousel", () =>
     it("renders correctly", () => {
         const tree = renderer
-            .create(<ShowcaseCarousel projects={[project]} />)
+            .create(
+                <MemoryRouter>
+                    <ShowcaseCarousel projects={[project]} />
+                </MemoryRouter>
+            )
             .toJSON();
         expect(tree).toMatchSnapshot();
     }));

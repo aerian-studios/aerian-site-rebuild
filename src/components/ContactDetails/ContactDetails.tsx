cfd0902f6cbd8bf8205467f03a86a9b3a3e2562f
sunya <sunya.dickman@aerian.com>
2018-07-10 20:37:44 +0100

    Fix ContactDetails

--- src/components/ContactDetails/ContactDetails.tsx
+++ src/components/ContactDetails/ContactDetails.tsx
@@ -12,16 +12,56 @@ interface Props {
     postcode?: string;
 }
 
-export const ContactDetails: React.SFC<Props> = ({ style, className, phoneNumber, street, locality, region, postcode }) => (
-    <div className={[styles.component, className].join(" ")} style={style} itemscope={true} itemtype="http://schema.org/Organization">
-        {phoneNumber && (<p className={styles.phoneNumber}>Call <span itemprop="telephone">{phoneNumber}</span></p>)}
-        {(street || locality || region || postcode) && <p className={styles.address} itemprop="address" itemscope={true} itemtype="http://schema.org/PostalAddress">
-            {street && (<span className={styles.part} itemprop="streetAddress">{street}</span>)}
-            {locality && (<span className={styles.part} itemprop="addressLocality">{locality}</span>)}
-            {region && (<span className={styles.part} itemprop="addressRegion">{region}</span>)}
-            {postcode && (<span className={styles.part} itemprop="postalCode">{postcode}</span>)}
-        </p>}
+export const ContactDetails: React.SFC<Props> = ({
+    style,
+    className,
+    phoneNumber,
+    street,
+    locality,
+    region,
+    postcode
+}) => (
+    <div
+        className={className}
+        style={style}
+        itemScope={true}
+        itemType="http://schema.org/Organization"
+    >
+        {phoneNumber && (
+            <p className={styles.phoneNumber}>
+                Call <span itemProp="telephone">{phoneNumber}</span>
+            </p>
+        )}
+        {(street || locality || region || postcode) && (
+            <p
+                className={styles.address}
+                itemProp="address"
+                itemScope={true}
+                itemType="http://schema.org/PostalAddress"
+            >
+                {street && (
+                    <span className={styles.part} itemProp="streetAddress">
+                        {street}
+                    </span>
+                )}
+                {locality && (
+                    <span className={styles.part} itemProp="addressLocality">
+                        {locality}
+                    </span>
+                )}
+                {region && (
+                    <span className={styles.part} itemProp="addressRegion">
+                        {region}
+                    </span>
+                )}
+                {postcode && (
+                    <span className={styles.part} itemProp="postalCode">
+                        {postcode}
+                    </span>
+                )}
+            </p>
+        )}
     </div>
 );
-   
+
 export default ContactDetails;

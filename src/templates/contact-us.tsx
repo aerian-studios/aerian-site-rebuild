7cf4ced2bcf5e9bd95727cbbd242aeb89d0c7d21
Matt Kane <ascorbic@gmail.com>
2018-07-12 16:06:08 +0100

    Stub out socialgrid and layout contact us page

--- src/templates/contact-us.tsx
+++ src/templates/contact-us.tsx
@@ -1,4 +1,5 @@
 import { graphql } from "gatsby";
+import "leaflet/dist/leaflet.css";
 import * as React from "react";
 import { ContactForm } from "../components/ContactForm";
 import { ContactInfo } from "../components/ContactInfo";
@@ -6,6 +7,8 @@ import Layout from "../components/Layout";
 import { MapView } from "../components/MapView";
 import { SocialGrid } from "../components/SocialGrid";
 import { Page, ReactRouterLocation } from "../types/data";
+
+import * as styles from "./contact-us.scss";
 interface GraphData {
     pagesJson: Page;
 }
@@ -18,11 +21,14 @@ interface Props {
 export const ContactUsPage: React.SFC<Props> = props => {
     return (
         <Layout location={props.location} {...props.data.pagesJson}>
-            <section>
-                <MapView />
-                <ContactInfo title={props.data.pagesJson.title} />
-                <ContactForm />
-                <SocialGrid />
+            <section className={styles.component}>
+                <MapView
+                    position={[51.4194618, -2.2542012]}
+                    className={styles.map}
+                />
+                {/* <ContactInfo title={props.data.pagesJson.title} /> */}
+                <ContactForm className={styles.contactForm} />
+                <SocialGrid className={styles.socialGrid} />
             </section>
         </Layout>
     );

44b445160b8bcaca6a23986c6f617c2af75b256b
Matt Kane <ascorbic@gmail.com>
2018-07-13 17:15:54 +0100

    Use fluid image for avatar

--- src/types/fragments.ts
+++ src/types/fragments.ts
@@ -144,8 +144,8 @@ export const projectFragment = graphql`
             title
             avatar {
                 childImageSharp {
-                    fixed(width: 290) {
-                        ...GatsbyImageSharpFixed
+                    fluid(maxWidth: 231) {
+                        ...GatsbyImageSharpFluid
                     }
                 }
                 publicURL

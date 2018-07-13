943c409a7e733ecaff66950be3534bdb1c087058
Darren Torchia <darren.torchia@aerian.com>
2018-07-13 16:33:48 +0100

    updates as per PR

--- src/components/TestimonialBlock/TestimonialBlock.tsx
+++ src/components/TestimonialBlock/TestimonialBlock.tsx
@@ -4,6 +4,10 @@ import { ImageField, Testimonial } from "../../types/data";
 import { Image } from "./../Image";
 import * as styles from "./TestimonialBlock.scss";
 
+import { faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 interface Props {
     style?: React.CSSProperties;
     className?: string;
@@ -11,28 +15,30 @@ interface Props {
 }
 
 function showImage(image?: ImageField) {
-    return(
+    return (
         image ? <Image source={image} /> : null
     )
 }
 
 export const TestimonialBlock: React.SFC<Props> = ({ children, style, className, testimonial }) => (
-    <div className={[styles.component, className].join(" ")} style={style}>
+    <div className={[styles.component, className].join(" ")} style={style} itemProp="review" itemScope={true} itemType="http://schema.org/Review">
         {showImage(testimonial.image)}
         <blockquote className={styles.quote}>
-            <div className={styles.testimonialText}>
+            <FontAwesomeIcon className={styles.quoteLeft} icon={faQuoteLeft} />
+            <div className={styles.testimonialText} itemProp="reviewBody">
                 {testimonial.quote}
             </div>
-            <footer>
+            <footer className={styles.testimonialFooter}>
                 <figure className={styles.blockquoteImage}>
                     {showImage(testimonial.avatar)}
                 </figure>
-                <span className={styles.nameTitle}>
-                    <span className={styles.name}>{testimonial.person}</span>
-                    <span className={styles.title}>{testimonial.title}</span>
-                </span>
+                <div className={styles.nameTitle}>
+                    <span className={styles.name} itemProp="author">{testimonial.person}</span>
+                    <span className={styles.title} itemProp="name">{testimonial.title}</span>
+                </div>
             </footer>
         </blockquote>
     </div>
 );
 export default TestimonialBlock;
+

1187f728ac382919a6b98dc608a940aaf707c3d4
Matt Kane <ascorbic@gmail.com>
2018-07-18 13:19:56 +0100

    Fix babel jest config

--- jestPreprocess.js
+++ jestPreprocess.js
@@ -1,3 +1,9 @@
-const babelOptions = { presets: ["react", "env"] };
+const babelOptions = {
+    presets: ["@babel/react", "@babel/env"],
+    plugins: [
+        "@babel/plugin-proposal-optional-chaining",
+        "@babel/plugin-proposal-class-properties"
+    ]
+};
 
 module.exports = require("babel-jest").createTransformer(babelOptions);

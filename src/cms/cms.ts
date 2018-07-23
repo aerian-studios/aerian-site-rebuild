1384c84264bef6670b4e17de67a4003dc566e91c
Matt Kane <ascorbic@gmail.com>
2018-07-23 10:21:56 +0100

    Change regsitration of styles

--- src/cms/cms.ts
+++ src/cms/cms.ts
@@ -1,14 +1,14 @@
 import CMS from "netlify-cms";
 import "netlify-cms/dist/cms.css";
 import * as React from "react";
-import "../lib/theme";
+// import "../lib/theme";
 
 // import preview templates for the CMS here
 // import AboutPagePreview from "./preview-templates/AboutPagePreview";
 import ProjectPagePreview from "./preview-templates/ProjectPagePreview";
 
 // // Styles for the CMS
-CMS.registerPreviewStyle("/styles.css");
+CMS.registerPreviewStyle("styles.css");
 
 // // Register preview templates
 // CMS.registerPreviewTemplate("what-we-do", AboutPagePreview);

051a3fed762b87ccce5d5b06f5c9f4b8bb4114df
Matt Kane <matt.kane@aerian.com>
2018-07-24 11:10:17 +0100

    Fix about page

--- src/components/AboutPage/AboutPage.tsx
+++ src/components/AboutPage/AboutPage.tsx
@@ -1,7 +1,9 @@
 import classNames from "classnames";
 import * as React from "react";
 
+import { Link } from "gatsby";
 import { About, Client } from "../../types/data";
+import { Block } from "../Block";
 import { ClientGridBlock } from "../ClientGridBlock";
 import { HeroBlock } from "../HeroBlock";
 import { Image } from "../Image";
@@ -14,29 +16,34 @@ interface Props {
 }
 
 export const AboutPage: React.SFC<Props> = ({ page, clients }) => (
-    <section>
+    <>
         <PageHeader>
-            <HeroBlock
-                heroImage={page.heroImage}
-                aria-labelled-by="page-title"
-            />
-            <div>
-                <h1>{page.title}</h1>
-            </div>
+            <HeroBlock heroImage={page.heroImage} aria-labelled-by="page-title">
+                <div>
+                    <h1>If you rely on the web you can rely on us</h1>
+                    <Link to={"/our-work"}>View our work</Link>
+                </div>
+            </HeroBlock>
         </PageHeader>
-        <ClientGridBlock clients={clients} />
-        <div style={{ display: "flex" }}>
-            {page.infographic &&
-                page.infographic.map(
-                    i =>
-                        i.image && (
-                            <Image
-                                style={{ width: 100, height: 100 }}
-                                source={i.image}
-                            />
-                        )
-                )}
-        </div>
-    </section>
+        <section className={styles.contentWrapper}>
+            <Block>
+                <h1>{page.subheading}</h1>
+                <p>{page.description}</p>
+            </Block>
+            <ClientGridBlock clients={clients} />
+            <div style={{ display: "flex" }}>
+                {page.infographic &&
+                    page.infographic.map(
+                        i =>
+                            i.image && (
+                                <Image
+                                    style={{ width: 100, height: 100 }}
+                                    source={i.image}
+                                />
+                            )
+                    )}
+            </div>
+        </section>
+    </>
 );
 export default AboutPage;

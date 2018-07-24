4dad8094c88fa5d6b22e3b12bfd00e2449ae38ea
Matt Kane <matt.kane@aerian.com>
2018-07-24 17:20:21 +0100

    Add storyshots

--- jestPreprocess.js
+++ jestPreprocess.js
@@ -2,7 +2,8 @@ const babelOptions = {
     presets: ["@babel/react", "@babel/env"],
     plugins: [
         "@babel/plugin-proposal-optional-chaining",
-        "@babel/plugin-proposal-class-properties"
+        "@babel/plugin-proposal-class-properties",
+        "require-context-hook"
     ]
 };
 

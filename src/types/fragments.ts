6a3012362185f6068fccb2c20be161764116f24f
Matt Kane <matt.kane@aerian.com>
2018-07-24 15:35:39 +0100

    Re-add traced svg

--- src/types/fragments.ts
+++ src/types/fragments.ts
@@ -24,7 +24,7 @@ export const clientFragment = graphql`
         logo {
             childImageSharp {
                 fluid(maxWidth: 420) {
-                    ...GatsbyImageSharpFluid
+                    ...GatsbyImageSharpFluid_tracedSVG
                 }
             }
             publicURL
@@ -32,7 +32,7 @@ export const clientFragment = graphql`
         promoLogo {
             childImageSharp {
                 fluid(maxWidth: 420) {
-                    ...GatsbyImageSharpFluid
+                    ...GatsbyImageSharpFluid_tracedSVG
                 }
             }
             publicURL

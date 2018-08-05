47b3981f8ed4798ea932010bb8aaa46254d157d7
Matt Kane <matt.kane@aerian.com>
2018-08-05 11:20:57 +0100

    Add contact form (WIP)

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 
+import { Button } from "../Button";
 import * as styles from "./ContactForm.module.scss";
 
 interface Props {
@@ -7,24 +8,104 @@ interface Props {
     className?: string;
 }
 interface State {
-    myStateValue?: boolean;
+    name: string;
+    email: string;
+    phone: string;
+    message: string;
 }
 
-const INITIAL_STATE: State = {
-    myStateValue: true
+const INITIAL_STATE: State = { name: "", email: "", phone: "", message: "" };
+
+const encode = (data: { [key: string]: string }) => {
+    return Object.keys(data)
+        .map(
+            key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+        )
+        .join("&");
 };
 
 export class ContactForm extends React.PureComponent<Props, State> {
     public state = INITIAL_STATE;
 
+    public handleChange = (
+        e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) =>
+        this.setState({ [e.target.name]: e.target.value } as Pick<
+            State,
+            keyof State
+        >);
+
+    public handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        this.sendMessage();
+    };
+
+    public sendMessage = async () => {
+        alert(encode({ "form-name": "contact", ...this.state }));
+        try {
+            await fetch("/", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },
+                body: encode({ "form-name": "contact", ...this.state })
+            });
+            alert("Sent message");
+        } catch (e) {
+            console.log("Error");
+        }
+    };
+
     public render() {
+        const { name, email, phone, message } = this.state;
         return (
-            <div
+            <form
+                onSubmit={this.handleSubmit}
                 className={[styles.component, this.props.className].join(" ")}
                 style={this.props.style}
+                data-netlify={true}
+                name="contact"
             >
-                <h2>ContactForm</h2>
-            </div>
+                <div>
+                    <label htmlFor="name">Name</label>
+                    <input
+                        name="name"
+                        type="text"
+                        value={name}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        name="email"
+                        type="email"
+                        value={email}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="phone">Telephone</label>
+                    <input
+                        name="phone"
+                        type="text"
+                        value={phone}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="message">Message</label>
+                    <textarea
+                        name="message"
+                        value={message}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <input type="hidden" name="form-name" value="contact" />
+                <Button arrow={true}>
+                    <button type="submit">Submit</button>
+                </Button>
+            </form>
         );
     }
 }

8667778d9abc18b5bd409fb7d2792ea1901fe0e6
sunya <sunya.dickman@aerian.com>
2018-08-09 15:06:31 +0100

    Tests for scrollIntoView

--- src/lib/scrollIntoView.test.ts
+++ src/lib/scrollIntoView.test.ts
@@ -1,29 +1,31 @@
 /// <reference types="@types/jest" />
-import * as renderer from "react-test-renderer";
 import * as scrollIntoView from "./scrollIntoView";
 
-it("Should not have a margin", () => {
-    const element = document.createElement("div");
-    element.style.cssText = "margin-top: 300px;";
-    document.body.appendChild(element);
+describe("scrollIntoView", () => {
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
 
-    const doScrollingSpy = jest.spyOn(scrollIntoView, "doScrolling");
+    it("Should not have a margin", () => {
+        const element = document.createElement("div");
+        element.style.cssText = "margin-top: 300px;";
+        document.body.appendChild(element);
 
-    scrollIntoView.scrollToElement(element, 0);
-    expect(doScrollingSpy).toHaveBeenCalledWith(0, 0);
-    // window.setTimeout(() => {
-    //     expect(document.body.scrollTop).toEqual(300);
-    // });
-});
-it("Shouldmemoize a margin", () => {
-    const element = document.createElement("div");
-    element.style.cssText = "margin-top: 300px;";
-    document.body.appendChild(element);
+        const doScrollingSpy = jest.spyOn(scrollIntoView, "doScrolling");
 
-    const doScrollingSpy = jest.spyOn(scrollIntoView, "doScrolling");
+        scrollIntoView.scrollToElement(element, 0);
+        expect(doScrollingSpy).toHaveBeenCalledWith(0, 0);
+    });
+    it("Shouldmemoize a margin", () => {
+        const element = document.createElement("div");
+        element.style.cssText = "margin-top: 300px;";
+        document.body.appendChild(element);
 
-    scrollIntoView.setTopMargin(300);
+        const doScrollingSpy = jest.spyOn(scrollIntoView, "doScrolling");
 
-    scrollIntoView.scrollToElement(element, 0);
-    expect(doScrollingSpy).toHaveBeenCalledWith(-300, 0);
+        scrollIntoView.setTopMargin(300);
+
+        scrollIntoView.scrollToElement(element, 0);
+        expect(doScrollingSpy).toHaveBeenCalledWith(-300, 0);
+    });
 });

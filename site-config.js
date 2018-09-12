f487390ebdaf6e93e6bb9f75d36c418209171616
Matt Kane <matt.kane@aerian.com>
2018-09-12 16:52:21 +0100

    Sanitise bucket key

--- site-config.js
+++ site-config.js
@@ -12,7 +12,10 @@ const themeColor = "#c62828"; // Used for setting manifest and progress theme co
 
 module.exports = {
     pathPrefix: process.env.CODEBUILD_SOURCE_VERSION
-        ? `public/${process.env.CODEBUILD_SOURCE_VERSION}`
+        ? `public/${process.env.CODEBUILD_SOURCE_VERSION.replace(
+              /[^\w]/g,
+              "-"
+          )}`
         : "/", // Prefixes all links. For cases when deployed to example.github.io/my-site/.
     // meta-data for <head>, used by https://github.com/nfl/react-helmet
     siteMetaDate: {

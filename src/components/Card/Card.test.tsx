d090b7f54941eb088abe74c48a8af45fd3c01e25
S-unya <sunya.dickman@aerian.com>
2018-12-07 15:58:31 +0000

    96: Tests for Card

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -15,14 +15,4 @@ describe("Card", () => {
         const tree = card.toJSON();
         expect(tree).toMatchSnapshot();
     });
-    it("renders visible cards", () => {
-        const card = renderer.create(
-            <Card>
-                <p>Hi</p>
-            </Card>
-        );
-        (card.getInstance() as any).setVisible(true);
-        const tree = card.toJSON();
-        expect(tree).toMatchSnapshot();
-    });
 });

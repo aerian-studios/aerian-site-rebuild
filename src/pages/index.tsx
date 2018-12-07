1b42a2d28470003e010ddb75cec7eeedf15c1106
S-unya <sunya.dickman@aerian.com>
2018-12-07 16:00:29 +0000

    96: Moved responsibiities from the index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
-import { graphql, Link } from "gatsby";
+import { graphql } from "gatsby";
 import * as React from "react";
-import { Button } from "../components/Button";
-import { Card } from "../components/Card";
-import { Image } from "../components/Image";
 import Layout from "../components/Layout";
 import { ShowcaseCarousel } from "../components/ShowcaseCarousel";
 import { extractNodes } from "../lib/helpers";
+
 import { NodeList, ProjectBox, ReactRouterLocation } from "../types/data";
 import * as styles from "./index.module.scss";
 interface Props {
@@ -26,48 +24,10 @@ const IndexPage: React.SFC<Props> = props => {
         Bespoke user driven design and software development with open source web technologies for over 20 years. `}
         >
             <section id="section-index">
-                <ShowcaseCarousel feature={true}>
-                    {extractNodes(props.data.allProjectsJson).map(
-                        (project, index) => (
-                            <Link
-                                key={project.slug}
-                                to={`/our-work/project/${project.slug}`}
-                                className={styles.revealCard}
-                            >
-                                <Image
-                                    key={project.titleLineOne}
-                                    fadeIn={index >= 7}
-                                    alt={project.name}
-                                    backgroundColor={`#d01944`}
-                                    source={
-                                        index === 0
-                                            ? project.heroImage
-                                            : project.thumbnail
-                                    }
-                                    className={styles.cardAlignment}
-                                />
-                                <Image
-                                    source={project.client.promoLogo}
-                                    alt={`${project.client.name}'s logo`}
-                                    className={styles.clientPromoLogo}
-                                />
-                                <div className={styles.revealCardContent}>
-                                    <h3 className={styles.cardTitle}>
-                                        {project.client.name}
-                                    </h3>
-                                    {index === 0 ? (
-                                        <p className={styles.cardContent}>
-                                            {project.titleLineOne}
-                                        </p>
-                                    ) : null}
-                                    <Button arrow={true} alternate={true}>
-                                        View project
-                                    </Button>
-                                </div>
-                            </Link>
-                        )
-                    )}
-                </ShowcaseCarousel>
+                <ShowcaseCarousel
+                    feature={true}
+                    data={extractNodes(props.data.allProjectsJson)}
+                />
             </section>
         </Layout>
     );

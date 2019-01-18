2274ea74c4560ce68d1011e5737ded781c2dc4a5
S-unya <sunya.dickman@aerian.com>
2019-01-18 12:54:09 +0000

    44: Usage changed according to API

--- src/components/ContactUsPage/ContactUsPage.tsx
+++ src/components/ContactUsPage/ContactUsPage.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { Page } from "../../types/data";
 import { ContactForm } from "../ContactForm";
-// import { ContactInfo } from "../ContactInfo";
+import { ContactInfo } from "../ContactInfo";
 import { MapView } from "../MapView";
 import { PageHeader } from "../PageHeader";
 import { SocialGrid } from "../SocialGrid";
@@ -16,7 +16,18 @@ export const ContactUsPage: React.SFC<Props> = ({ page }) => (
         <PageHeader />
         <section className={styles.component}>
             <MapView position={[51.41942, -2.253581]} className={styles.map} />
-            {/* <ContactInfo title={props.data.pagesJson.title} /> */}
+            <ContactInfo
+                title="Drop in and say hi"
+                phoneTitle="Give us a call"
+                phoneNumber="0345 408 6009"
+                organisationName="Aerian"
+                street="The Old Malthouse, Mill Lane"
+                locality="Box"
+                region="Wiltshire"
+                postcode="SN13 8PN"
+                emailTitle="Send us a message"
+                email="info@aerian.com"
+            />
             <ContactForm className={styles.contactForm} />
             <SocialGrid className={styles.socialGrid} />
         </section>

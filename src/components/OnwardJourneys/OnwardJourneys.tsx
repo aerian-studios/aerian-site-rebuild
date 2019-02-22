5e95d13e31a377a970e5e5d11f90f6db9046172b
S-unya <sunya.dickman@aerian.com>
2019-02-22 16:57:22 +0000

    26: OnwardJourneys now uses LinkButton

--- src/components/OnwardJourneys/OnwardJourneys.tsx
+++ src/components/OnwardJourneys/OnwardJourneys.tsx
@@ -1,5 +1,6 @@
-import { Link } from "gatsby";
+import classNames from "classnames";
 import * as React from "react";
+import { LinkButton } from "../LinkButton";
 import * as styles from "./OnwardJourneys.module.scss";
 
 interface Props {
@@ -13,14 +14,26 @@ export const OnwardJourneys: React.SFC<Props> = ({
     style,
     className
 }) => (
-    <div className={[styles.component, className].join(" ")} style={style}>
-        <div>
-            <h2>Like what you see?</h2>
-            <a href={projectURL}>Launch project</a>
+    <div className={classNames(styles.component, className)} style={style}>
+        <div className={classNames(styles.section, styles.firstSection)}>
+            <div className={styles.sectionContent}>
+                <h3 className={classNames("large-title", styles.largeTitle)}>
+                    Like what you see?
+                </h3>
+                <LinkButton to="/our-work" alternate={true} arrow={true}>
+                    View more work
+                </LinkButton>
+            </div>
         </div>
-        <div>
-            <h2>Got a project in mind?</h2>
-            <Link to="/contact-us">Contact us</Link>
+        <div className={classNames(styles.section, styles.secondSection)}>
+            <div className={styles.sectionContent}>
+                <h3 className={classNames("large-title", styles.largeTitle)}>
+                    Got a project in mind?
+                </h3>
+                <LinkButton to="/contact-us" alternate={true} arrow={true}>
+                    Contact us
+                </LinkButton>
+            </div>
         </div>
     </div>
 );

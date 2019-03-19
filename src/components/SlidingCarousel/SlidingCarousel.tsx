e16628b120aac522fec23cb9b7d7bcad09c0f79f
Sunya Dickman <sunya.dickman@aerian.com>
2019-03-19 15:46:46 +0000

    27: WIP: some work around control

--- src/components/SlidingCarousel/SlidingCarousel.tsx
+++ src/components/SlidingCarousel/SlidingCarousel.tsx
@@ -3,53 +3,33 @@ import * as React from "react";
 
 import * as styles from "./SlidingCarousel.module.scss";
 
-export interface SliderProps extends React.ComponentProps<React.FC> {
+export interface SliderProps extends React.ComponentPropsWithRef<React.FC> {
     style?: React.CSSProperties;
-    wrapperClassName?: string;
-    sliderClassName?: string;
-    // Centre the slider on the central slide. Defaults to true
-    center?: boolean;
-    // Gap between items expects to have units as well
-    itemGap?: string;
     // Infinite scrolling. Defaults to false
     infinite?: boolean;
+    ref?: any;
 }
-
-/**
- * Utility for setting CSS variables on the root node
- * @param {String} propertyName - String name of the CSS variable - e.g. --SCCardHeight
- * @param {String} propertyValue - value to set
- * @returns {void}
- */
-const setCSSVariable = (propertyName: string, propertyValue: string): void => {
-    const body = document.body;
-
-    if (!body) {
-        return;
-    }
-
-    body.style.setProperty(propertyName, propertyValue);
-};
 
 /**
  * Duplicate React children with their props
  * @param {React.ReactNode} children - The elements to duplicate with props
- * @returns {React.ReactNode[]} - Returns the duplicated children
+ * @param {object} injectingProps - Properties to add to the children
+ * @return {React.ReactNode[]} - Returns the duplicated children
  */
 const createDuplicateChildren = (
-    children: React.ReactNode
-): React.ReactNode[] =>
-    React.Children.map(children, child => {
+    children: React.ReactNode,
+    injectingProps?: {}
+): React.ReactNode[] => {
+    return React.Children.map(children, child => {
         if (!React.isValidElement(child)) {
             return child;
         }
-
         const childProps: React.PropsWithChildren<{
             children?: React.ReactNode;
         }> = {
-            children: null
+            children: null,
+            ...injectingProps
         };
-
         childProps.children = createDuplicateChildren(
             (child as React.ReactElement<{
                 children?: React.ReactNode;
@@ -57,122 +37,36 @@ const createDuplicateChildren = (
         );
         return React.cloneElement(child, childProps);
     });
+};
 
 /**
  * Sliding carousel Proper
  * @param {SliderProps} props - The FunctionalComponent props
- * @returns {React.FC} - SlidingCarousel Component
+ * @param {React.Ref<HTMLDivElement>} ref - The ref to forward
+ * @returns {React.FC<SliderProps>} - SlidingCarousel Component
  */
-export const SlidingCarousel: React.FC<SliderProps> = ({
-    children,
-    style,
-    wrapperClassName,
-    sliderClassName,
-    center = true,
-    infinite = false,
-    itemGap = "0px"
-}) => {
-    let ticking = false; // used for some render optimisations
-    let originalWidth = 1; // used largely in infinite scroll
-    const sliderRef = React.useRef<HTMLDivElement>(null);
-
-    /**
-     * Method to check if the infinite scroll needs to loop
-     * @param {number} position - the current scroll position
-     * @return {void}
-     */
-    const checkForLoop = (position: number) => {
-        if (!sliderRef || !sliderRef.current) {
-            return;
-        }
-
-        if (position < originalWidth) {
-            sliderRef.current.style.scrollBehavior = "auto";
-            sliderRef.current.scroll(position + originalWidth, 0);
-            sliderRef.current.style.scrollBehavior = "smooth";
-        } else if (position > originalWidth * 2) {
-            sliderRef.current.style.scrollBehavior = "auto";
-            sliderRef.current.scroll(position - originalWidth, 0);
-            sliderRef.current.style.scrollBehavior = "smooth";
-        }
-    };
-
-    /**
-     * Handle scroll events by calling necessary checks
-     * @param {Event} e Scroll event
-     * @return {void}
-     */
-    const scrollHandler = (e: Event) => {
-        if (!sliderRef || !sliderRef.current) {
-            return;
-        }
-        const lastScrollX = sliderRef.current.scrollLeft;
-
-        // tslint:disable-next-line
-        if (!ticking) {
-            window.requestAnimationFrame(() => {
-                if (infinite) {
-                    checkForLoop(lastScrollX);
-                }
-                ticking = false;
-            });
-
-            ticking = true;
-        }
-    };
-
-    // HOOKS
-    /**
-     * Set some init code to workout original width
-     */
-    React.useLayoutEffect(() => {
-        if (!infinite || !sliderRef || !sliderRef.current) {
-            return;
-        }
-        originalWidth = sliderRef.current.scrollWidth / 3;
-    });
-
-    /**
-     * Add scroll cheking on slider
-     */
-    React.useEffect(() => {
-        if (!sliderRef || !sliderRef.current) {
-            return;
-        }
-        // set scroll position to the middle
-        sliderRef.current.scroll(originalWidth, 0);
-        sliderRef.current.addEventListener("scroll", scrollHandler);
-
-        // Remove the event listener on destroy
-        return () => {
-            // tslint:disable-next-line
-            sliderRef.current &&
-                sliderRef.current.removeEventListener("scroll", scrollHandler);
-        };
-    }, [sliderRef.current]);
-
-    /**
-     * Set some CSS variables based on props
-     */
-    React.useEffect(() => {
-        setCSSVariable("--SCSnapAlign", center ? "center" : "start");
-        setCSSVariable("--SCGridGap", itemGap);
-    });
+export const SlidingCarousel: React.RefForwardingComponent<
+    React.Ref<HTMLDivElement>,
+    SliderProps
+> = React.forwardRef((props: SliderProps, ref?: React.Ref<HTMLDivElement>) => {
+    const { children, style, infinite = false } = props;
 
     return (
-        <section
-            className={classNames(styles.sliderWrapper, wrapperClassName)}
-            style={style}
-        >
-            <div
-                className={classNames(sliderClassName, styles.SCSlider)}
-                ref={sliderRef}
-            >
-                {infinite ? createDuplicateChildren(children) : children}
+        <section className={classNames(styles.sliderWrapper)} style={style}>
+            <div className={classNames(styles.SCSlider)} ref={ref}>
+                {infinite
+                    ? createDuplicateChildren(children, {
+                          "aria-hidden": "true"
+                      })
+                    : children}
                 {children}
-                {infinite ? createDuplicateChildren(children) : children}
+                {infinite
+                    ? createDuplicateChildren(children, {
+                          "aria-hidden": "true"
+                      })
+                    : children}
             </div>
         </section>
     );
-};
+});
 export default SlidingCarousel;
--- src/components/SlidingCarousel/SlidingCarouselProvider.tsx
+++ src/components/SlidingCarousel/SlidingCarouselProvider.tsx
@@ -5,28 +5,302 @@ import { SliderProps, SlidingCarousel } from "./SlidingCarousel";
 
 import * as styles from "./SlidingCarousel.module.scss";
 
+interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {}
+export type ButtonRenderProp<P extends ButtonProps = ButtonProps> = (
+    button: React.JSXElementConstructor<P>
+) => React.ReactElement;
+
 interface Props extends SliderProps {
     className?: string;
     style?: React.CSSProperties;
-    buttonBackContent?: React.ReactElement;
-    buttonFwdContent?: React.ReactElement;
+    buttonBackContentRender?: ButtonRenderProp;
+    buttonFwdContentRender?: ButtonRenderProp;
+    center?: boolean;
+    infinite?: boolean;
+    itemGap?: string;
 }
+
+interface Size {
+    width: number;
+    height: number;
+}
+
+/**
+ * Utility for setting CSS variables on the root node
+ * @param {String} propertyName - String name of the CSS variable - e.g. --SCCardHeight
+ * @param {String} propertyValue - value to set
+ * @returns {void}
+ */
+const setCSSVariable = (propertyName: string, propertyValue: string): void => {
+    const body = document.body;
+
+    if (!body) {
+        return;
+    }
+
+    body.style.setProperty(propertyName, propertyValue);
+};
+
+const workOutSizes = (nodes: HTMLCollection): Size[] => {
+    const len = nodes.length;
+    const ret: Size[] = [];
+
+    if (len) {
+        for (let i = 0; i < len; i++) {
+            const el: Element = nodes[i];
+
+            ret.push({ width: el.clientWidth, height: el.clientHeight });
+        }
+    }
+    return ret;
+};
+
 export const SlidingCarouselProvider: React.FC<Props> = ({
     children,
     className,
     style,
-    buttonBackContent,
-    buttonFwdContent,
-    ...rest
+    buttonBackContentRender,
+    buttonFwdContentRender,
+    center = true,
+    infinite = false,
+    itemGap = "0px"
 }) => {
+    const sliderRef = React.useRef<HTMLDivElement>();
+    let stillInFrame = false; // used for some render optimisations
+    let originalWidth = 1; // used largely in infinite scroll
+    let pageWidth = 200;
+
+    const centerThingsNearestToPosition = (positionToCentre?: number) => {
+        if (!sliderRef || !sliderRef.current) {
+            return;
+        }
+
+        const position =
+            typeof positionToCentre !== "undefined"
+                ? positionToCentre
+                : sliderRef.current.scrollLeft;
+        let tempPosition = 0;
+        let i = 0;
+        let index = 0;
+
+        // assume that the children have non-uniform widths
+        while (tempPosition <= position) {
+            const elW = childSizes[i].width;
+            i++;
+
+            // @TODO: need to sort out page centring
+            if (
+                (center && tempPosition + elW / 2 >= position) ||
+                tempPosition + elW >= position
+            ) {
+                tempPosition += center ? elW / 2 : elW;
+                index = i - 1;
+            } else {
+                tempPosition += childSizes[i].width;
+            }
+        }
+
+        sliderRef.current.scroll(tempPosition, 0);
+
+        if (index !== current) {
+            setCurrent(index);
+        }
+    };
+
+    const centerTheThingsToIndex = (indexToCentre?: number) => {
+        if (!childSizes.length || !sliderRef || !sliderRef.current) {
+            return;
+        }
+
+        const index = indexToCentre ? indexToCentre : current;
+        let scrollAmount = center ? pageWidth * 0.5 : 0;
+
+        for (let i = 0; i < index; i++) {
+            const elW = childSizes[i].width;
+
+            scrollAmount += elW;
+
+            if (center && i === index - 1) {
+                scrollAmount -= elW;
+            }
+        }
+
+        sliderRef.current.scroll(scrollAmount, 0);
+
+        if (index !== current) {
+            setCurrent(index);
+        }
+    };
+
+    const centerTheThings = () => {
+        centerTheThingsToIndex(current);
+    };
+
+    const moveCarouselPage = (direction: number) => {
+        if (!sliderRef || !sliderRef.current) {
+            return;
+        }
+
+        const currentPosition = sliderRef.current.scrollLeft;
+
+        // TODO: need to do a loop check here
+        sliderRef.current.scroll(currentPosition + pageWidth * direction, 0);
+    };
+
+    /**
+     * Method to check if the infinite scroll needs to loop
+     * @param {number} position - the current scroll position
+     * @return {void}
+     */
+    const checkForLoop = (position: number) => {
+        if (!sliderRef || !sliderRef.current) {
+            return;
+        }
+
+        if (position < originalWidth) {
+            sliderRef.current.style.scrollBehavior = "auto";
+            sliderRef.current.scroll(position + originalWidth, 0);
+            sliderRef.current.style.scrollBehavior = "smooth";
+        } else if (position > originalWidth * 2) {
+            sliderRef.current.style.scrollBehavior = "auto";
+            sliderRef.current.scroll(position - originalWidth, 0);
+            sliderRef.current.style.scrollBehavior = "smooth";
+        }
+    };
+
+    /**
+     * Handle scroll events by calling necessary checks
+     * @param {Event} e Scroll event
+     * @return {void}
+     */
+    const scrollHandler = (e: Event) => {
+        if (!sliderRef || !sliderRef.current) {
+            return;
+        }
+        const lastScrollX = sliderRef.current.scrollLeft;
+
+        // tslint:disable-next-line
+        if (!stillInFrame) {
+            window.requestAnimationFrame(() => {
+                if (infinite) {
+                    checkForLoop(lastScrollX);
+                }
+                stillInFrame = false;
+            });
+
+            stillInFrame = true;
+        }
+    };
+
+    // HOOKS
+    /**
+     * Set some init code to workout original width
+     */
+    let childSizes: Size[] = [];
+    const [current, setCurrent] = React.useState(0);
+    React.useLayoutEffect(() => {
+        if (!infinite || !sliderRef || !sliderRef.current) {
+            return;
+        }
+        originalWidth = sliderRef.current.scrollWidth / 3;
+        pageWidth = window.innerWidth;
+
+        childSizes = workOutSizes(sliderRef.current.children);
+        const originalLength = childSizes.length / 3;
+
+        console.log("now");
+
+        // centerTheThingsToIndex(
+        //     center ? Math.floor(originalLength + originalLength / 2) : 0
+        // );
+
+        window.setTimeout(() => {
+            centerTheThingsToIndex(
+                center ? Math.floor(originalLength + originalLength / 2) : 0
+            );
+        }, 100);
+    });
+
+    /**
+     * Add scroll cheking on slider
+     */
+    React.useEffect(() => {
+        if (!sliderRef || !sliderRef.current) {
+            return;
+        }
+        // set scroll position to the middle
+        sliderRef.current.scroll(originalWidth, 0);
+        sliderRef.current.addEventListener("scroll", scrollHandler);
+
+        // Remove the event listener on destroy
+        return () => {
+            // tslint:disable-next-line
+            sliderRef.current &&
+                sliderRef.current.removeEventListener("scroll", scrollHandler);
+        };
+    }, [sliderRef.current]);
+
+    React.useEffect(() => {
+        window.addEventListener("resize", centerTheThings);
+
+        return () => {
+            window.removeEventListener("resize", centerTheThings);
+        };
+    });
+
+    /**
+     * Set some CSS variables based on props
+     */
+    React.useEffect(() => {
+        setCSSVariable("--SCSnapAlign", center ? "center" : "start");
+        setCSSVariable("--SCGridGap", itemGap);
+    });
+
+    const FwdButton: React.SFC<ButtonProps> = props => (
+        <button
+            aria-hidden="true"
+            tabIndex={-1}
+            {...props}
+            onClick={() => {
+                if (!sliderRef || !sliderRef.current) {
+                    return;
+                }
+
+                moveCarouselPage(1);
+            }}
+        />
+    );
+    const BackButton: React.SFC<ButtonProps> = props => (
+        <button
+            aria-hidden="true"
+            tabIndex={-1}
+            {...props}
+            onClick={() => {
+                if (!sliderRef || !sliderRef.current) {
+                    return;
+                }
+
+                moveCarouselPage(-1);
+            }}
+        />
+    );
+    const fwdButton = buttonFwdContentRender
+        ? buttonFwdContentRender(FwdButton)
+        : null;
+    const backButton = buttonBackContentRender
+        ? buttonBackContentRender(BackButton)
+        : null;
+
     return (
         <div
             className={classNames(className, styles.sliderProvider)}
             style={style}
         >
-            <SlidingCarousel {...rest}>{children}</SlidingCarousel>
-            {buttonBackContent ? <button>{buttonBackContent}</button> : null}
-            {buttonFwdContent ? <button>{buttonFwdContent}</button> : null}
+            <SlidingCarousel infinite={infinite} ref={sliderRef}>
+                {children}
+            </SlidingCarousel>
+            {backButton}
+            {fwdButton}
         </div>
     );
 };

1bb287b9308e6b5238299658c2dd288e9ae21a96
Sunya Dickman <sunya.dickman@aerian.com>
2019-04-03 07:57:02 +0100

    27: WIP: Fill in hole in test

--- src/components/SlidingCarousel/SlidingCarouselProvider.test.tsx
+++ src/components/SlidingCarousel/SlidingCarouselProvider.test.tsx
@@ -13,10 +13,10 @@ import { Size } from "./SlidingCarouselProvider";
 
 describe("SlidingCarouselProvider", () => {
     const childSizes: Size[] = [
-        { width: 106, height: 306 },
-        { width: 206, height: 306 },
-        { width: 306, height: 306 },
-        { width: 406, height: 306 }
+        { width: 106, height: 306 }, // 0
+        { width: 206, height: 306 }, // 106
+        { width: 306, height: 306 }, // 312
+        { width: 406, height: 306 } // 612
     ];
 
     it("renders correctly", () => {
@@ -93,5 +93,41 @@ describe("SlidingCarouselProvider", () => {
         expect(second).toEqual(-300 + 312 + childSizes[2].width / 2);
         expect(third).toEqual(-300 + 312 + childSizes[2].width / 2);
         expect(fourth).toEqual(-300 + 618 + childSizes[3].width / 2);
+    });
+
+    it("returns the same result for index and position, left aligned", () => {
+        const snap1 = calculateNearestSnapPoint(0, childSizes, 0);
+        const snap2 = calculateNearestSnapPoint(300, childSizes, 0);
+        const snap3 = calculateNearestSnapPoint(309, childSizes, 0);
+        const snap4 = calculateNearestSnapPoint(600, childSizes, 0);
+        const snap5 = calculateNearestSnapPoint(100, childSizes, 0);
+        const index1 = calculateScrollOffsetForIndex(0, childSizes, 0);
+        const index2 = calculateScrollOffsetForIndex(1, childSizes, 0);
+        const index3 = calculateScrollOffsetForIndex(2, childSizes, 0);
+        const index4 = calculateScrollOffsetForIndex(3, childSizes, 0);
+
+        expect(snap1).toEqual(index1);
+        expect(snap5).toEqual(index2);
+        expect(snap2).toEqual(index3);
+        expect(snap3).toEqual(index3);
+        expect(snap4).toEqual(index4);
+    });
+
+    it("returns the same result for index and position, centre aligned", () => {
+        const snap1 = calculateNearestSnapPoint(0, childSizes, 300);
+        const snap2 = calculateNearestSnapPoint(300, childSizes, 300);
+        const snap3 = calculateNearestSnapPoint(309, childSizes, 300);
+        const snap4 = calculateNearestSnapPoint(600, childSizes, 300);
+        const snap5 = calculateNearestSnapPoint(100, childSizes, 300);
+        const index1 = calculateScrollOffsetForIndex(0, childSizes, 300);
+        const index2 = calculateScrollOffsetForIndex(1, childSizes, 300);
+        const index3 = calculateScrollOffsetForIndex(2, childSizes, 300);
+        const index4 = calculateScrollOffsetForIndex(3, childSizes, 300);
+
+        expect(snap1).toEqual(index1);
+        expect(snap5).toEqual(index2);
+        expect(snap2).toEqual(index3);
+        expect(snap3).toEqual(index3);
+        expect(snap4).toEqual(index4);
     });
 });

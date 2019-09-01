3c9029926f4297adc47ebebceff8988e0d87cc30
Matt Kane <matt.kane@aerian.com>
2019-09-01 18:35:05 +0100

    Switch from ts-jest. Breaks one test

--- jestPreprocess.js
+++ jestPreprocess.js
@@ -1,8 +1,11 @@
+/* eslint-disable */
 const babelOptions = {
-    presets: ["@babel/react", "@babel/env"],
+    presets: ["@babel/react", "@babel/env", "@babel/preset-typescript"],
     plugins: [
         "@babel/plugin-proposal-optional-chaining",
         "@babel/plugin-proposal-class-properties",
+        "babel-plugin-remove-graphql-queries",
+        "@babel/plugin-transform-runtime",
         "require-context-hook"
     ]
 };
